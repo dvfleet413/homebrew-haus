e7030a978739e0253d8c07debf8d5cf76fcf832e
github username <github email address>
2020-05-28 00:05:59 -0400

    started setting up router

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import manageRecipes from './reducers/manageRecipes'
@@ -11,7 +12,9 @@ const store = createStore(manageRecipes, window.__REDUX_DEVTOOLS_EXTENSION__ && 
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <Route path="/" component={App} />
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
