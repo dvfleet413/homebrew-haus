4bf31378b0241779105a77d1db2d577adac691b9
github username <github email address>
2020-05-30 17:12:18 -0400

    improvements to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,14 +10,18 @@ const Home = () => {
                     <img src={homeBrewHaus} alt="logo" />
                 </div>
                 <div className="bottom-left">
+                    <h5>Recipes</h5>
                     <Link to='/recipes' >View All Recipes</Link> <br />
-                    <Link to='/recipes/new' >Add a New Recipe</Link>
+                    <Link to='/recipes/new' >Add a New Recipe</Link><br />
+                    <br />
+                    <h5>Account</h5>
+                    <Link to='/login'>Login</Link>
                 </div>
             </div>
             <div className="col-two">
                 <div className="right">
                     <h5>HomeBrew.haus</h5>
-                    <p>HomeBrew.haus is here to help you find and store your favorite homebrew recipes. Browse our list of recipes, or add your own! Enjoy your homebrew!</p>
+                    <p>HomeBrew.haus is here to help you find and store your favorite homebrew recipes. Browse our list of recipes, or add your own! And remember - <em>Enjoy your homebrew!</em></p>
                 </div>
             </div>
         </div>
