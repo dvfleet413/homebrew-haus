14ef0eaecb61cae52285e50fdd83743f0b45a6d2
github username <github email address>
2020-05-30 17:08:00 -0400

    working on home page and styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import homeBrewHaus from '../assets/homeBrewHaus.png';
 
 const Home = () => {
     return(
-        <div>
-            Home Component
+        <div className="home-container">
+            <div className="col-one">
+                <div className="top-left">
+                    <img src={homeBrewHaus} alt="logo" />
+                </div>
+                <div className="bottom-left">
+                    <Link to='/recipes' >View All Recipes</Link> <br />
+                    <Link to='/recipes/new' >Add a New Recipe</Link>
+                </div>
+            </div>
+            <div className="col-two">
+                <div className="right">
+                    <h5>HomeBrew.haus</h5>
+                    <p>HomeBrew.haus is here to help you find and store your favorite homebrew recipes. Browse our list of recipes, or add your own! Enjoy your homebrew!</p>
+                </div>
+            </div>
         </div>
     )
 }
