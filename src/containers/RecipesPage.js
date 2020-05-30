1ab0c114c81abbf66682dfaa4fee872cf53cfaf8
github username <github email address>
2020-05-30 14:01:31 -0400

    added link on recipes index to page to new recipe form

--- src/containers/RecipesPage.js
+++ src/containers/RecipesPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import RecipeSummary from '../components/RecipeSummary';
 
 
@@ -9,6 +10,7 @@ class RecipesPage extends Component {
         const recipes = this.props.recipes.map(recipe => <RecipeSummary key={`${recipe.id}-recipe`} recipe={recipe} match={this.props.match}/>)
         return(
             <div className="recipe-container">
+                <Link to={`/recipes/new`} className="btn btn-primary">Add New Recipe</Link>
                 {recipes}
             </div>
         )
