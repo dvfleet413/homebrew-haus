e7350ab3cb4eb602a805c96d551c1b92d91b128b
github username <github email address>
2020-06-05 12:04:58 -0400

    updates to navbar

--- src/components/AuthenticatedMenu.js
+++ src/components/AuthenticatedMenu.js
@@ -1,23 +1,32 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import Navbar from 'react-bootstrap/Navbar'
+import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import { LinkContainer } from 'react-router-bootstrap';
 
 const UnauthenticatedMenu = (props) => {
     return(
-        <Navbar bg="light">
+        <Navbar collapseOnSelect expand="md" bg="light">
             <LinkContainer to="/">
-                    <Navbar.Brand>HomeBrew.pub</Navbar.Brand>
-                </LinkContainer>
-            <Nav className="mr-auto">
-                <NavLink  to="/recipes" className="nvbar-link">Recipes</NavLink>
-            </Nav>
-            <Nav className="ml-auto">
-                <span>Hello, {props.user.name}!</span>
-                <NavLink to={`/users/${props.user.id}/recipes`} className="navbar-link">Your Recipes</NavLink>
-                <NavLink to="/logout" onClick={event => props.handleClick(event)} className="navbar-link">Logout</NavLink>
-            </Nav>
+                <Navbar.Brand>HomeBrew.pub</Navbar.Brand>
+            </LinkContainer>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+
+                <Nav className="mr-auto">
+                    <NavDropdown title="Recipes" id="collasible-nav-dropdown">
+                        <NavLink  to="/recipes" className="nvbar-link">Recipes</NavLink>
+                        <NavLink to={`/users/${props.user.id}/recipes`} className="navbar-link">Your Recipes</NavLink>
+                    </NavDropdown>
+                </Nav>
+                
+                <br />
+                <Nav className="ml-auto">
+                    <span>Hello, {props.user.name}!</span>
+                    <NavLink to="/logout" onClick={event => props.handleClick(event)} className="navbar-link">Logout</NavLink>
+                </Nav>
+            </Navbar.Collapse>
         </Navbar>
     )
 }
